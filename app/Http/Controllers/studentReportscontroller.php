a003d96c13260aed74e1ca368ee97f421c25fee9
grimmjow22 <mamarvinadan@gmail.com>
2024-03-14 11:59:34 -0500

    Pdf

--- app/Http/Controllers/studentReportscontroller.php
+++ app/Http/Controllers/studentReportscontroller.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Illuminate\Support\Facades\App;
+
 
 class studentReportscontroller extends Controller
 {
     public function show_cardex($id){
         $student = Student::find($id);
-        dd($student);
-        return view('cardex', compact('student'));
+        // dd($student);}
+        $data=[
+            'title'=>'Reporte Del Estudiante',
+            'details'=>$student
+        ];
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->loadView('reports.cardex', $data);
+        return $pdf->stream(); 
 
     }
 }
